497851ccc63c1e672910956631a5f46cfe3c73df
Najim473 <najim7304@gmail.com>
2023-11-14 19:45:41 +0600

    reinstalled vite

--- scripts/countProducts.js
+++
@@ -1,8 +0,0 @@
-import {message} from "./message.js";
-
-export const sum =(x,y)=>{
-    return x+y;
-}
-const sumOf = document.getElementById('sumOf');
-
-sumOf.innerHTML = sum();
--- scripts/message.js
+++
@@ -1,8 +0,0 @@
-
-export const message = ()=>{
-    const name = "Najim";
-    const age = 24;
-    return name +' is ' + age + ' years old';
-}
-
-
